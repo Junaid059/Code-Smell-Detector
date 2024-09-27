21757eb04da42767004ef3a1ee7f8962a34433b2
junaid khalid <junaidkhalid2001@gmail.com>
2024-09-27 16:54:41 +0500

    installed checkstyle and google checks for java code smells

--- backend/detectors/detectJavaSmells.js
+++ backend/detectors/detectJavaSmells.js
@@ -1,16 +1,15 @@
-import exec from 'child_process';
+import { exec } from 'child_process';
 import path from 'path';
 
 const detectJavaSmells = (filepath, res) => {
-      // Update the version if needed
-    const checkstylePath = path.resolve('checkstyle-10.3.3-all.jar');  
-    const rulesFilePath = path.resolve('google_checks.xml');  
+    const checkstylePath = path.resolve('checkstyle-10.3.3-all.jar');
+    const rulesFilePath = path.resolve('google_checks.xml');
 
-    exec(`java -jar ${checkstylePath} -c ${rulesFilePath} ${filepath}`, (error, stdout, stderr) => {
+    exec(`java -jar "${checkstylePath}" -c "${rulesFilePath}" "${filepath}"`, { maxBuffer: 1024 * 1024 }, (error, stdout, stderr) => {
         if (error) {
-            return res.status(500).send(stderr);
+            return res.status(500).send(`Error: ${stderr}`);
         }
-        res.send(stdout);  
+        res.send(stdout);
     });
 };
 
